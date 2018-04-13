0b333d17591086c8c2161ac42fa070ab0a8861e1
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-12 22:15:06 -0600

    added all vis files and got basic timeline running

---
+++ app.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('visualization');
+
+// Create a DataSet (allows two way data-binding)
+const items = new vis.DataSet([
+  { id: 1, content: 'Do Stuff', start: '2018-04-10', end: '2018-04-20' },
+  { id: 2, content: 'item 2', start: '2018-04-14' },
+  { id: 3, content: 'item 3', start: '2018-04-18' },
+  { id: 4, content: 'item 4', start: '2018-04-16', end: '2018-04-19' },
+  { id: 5, content: 'item 5', start: '2018-04-25' },
+  { id: 6, content: 'item 6', start: '2018-04-27' }
+]);
+
+// Configuration for the Timeline
+const options = {};
+
+// Create a Timeline
+const timeline = new vis.Timeline(container, items, options);
